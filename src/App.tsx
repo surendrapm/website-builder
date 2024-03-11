3e5d8ee11d36552933a07b1e9d517e3f83373283
surendra <suredon456@gmail.com>
2024-03-11 21:39:56 +0530

    basic block set and form plugins

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,40 @@
 import grapesjs, { Editor } from "grapesjs";
 import "grapesjs/dist/css/grapes.min.css";
 import { useEffect, useState } from "react";
+import plugin from "grapesjs-blocks-basic";
+import gjsForms from "grapesjs-plugin-forms";
 
 function App() {
   const [editor, setEditor] = useState<Editor | null>(null);
+
   useEffect(() => {
     const editor = grapesjs.init({
-      container: "#gjs",
+      container: "#editor",
       fromElement: true,
-      blockManager: {
-        appendTo: "#gjs",
-        blocks: [
-          {
-            id: "section", // id is mandatory
-            label: "<b>Section</b>", // You can use HTML/SVG inside labels
-            attributes: { class: "gjs-block-section" },
-            content: `<section>
-          <h1>This is a simple title</h1>
-          <div>This is just a Lorem text: Lorem ipsum dolor sit amet</div>
-        </section>`,
-          },
-          {
-            id: "text",
-            label: "Text",
-            content: '<div data-gjs-type="text">Insert your text here</div>',
-          },
-          {
-            id: "image",
-            label: "Image",
-            // Select the component once it's dropped
-            select: true,
-            // You can pass components as a JSON instead of a simple HTML string,
-            // in this case we also use a defined component type `image`
-            content: { type: "image" },
-            // This triggers `active` event on dropped components and the `image`
-            // reacts by opening the AssetManager
-            activate: true,
-          },
-        ],
+
+      plugins: [plugin,gjsForms],
+      pluginsOpts: {
+        [plugin]: {
+          /* options */
+        },
       },
+
       storageManager: false,
-      panels: { defaults: [] },
     });
     setEditor(editor);
-
-
-
   }, []);
 
   return (
     <div>
-      <h1>My react app with grapejs</h1>
-      <div id="gjs"  style={{
-         border:"3px solid #444"
-      }}>
-        <h1>hello world</h1>
+      <h1>My React App with GrapeJS</h1>
+      <div
+        id="editor"
+        style={{
+          height: "0px",
+          overflow: "hidden",
+        }}
+      >
+        <h1>Hello World</h1>
       </div>
     </div>
   );
